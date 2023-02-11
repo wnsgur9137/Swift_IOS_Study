65b3782bf3640067846b47664d26327aa199ac73
Junhyeok <wnsgur9137@naver.com>
2023-02-11 23:54:15 +0900

    Coordinator

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var lampImage: UIImageView!
     
-    
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
